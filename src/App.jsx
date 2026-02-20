1a03d8fdcaeef0e0f09c53e5637a576a9f40f41e
Riavbz <116383128+Riavbz@users.noreply.github.com>
2026-02-20 01:56:08 -0500

    Revert "Design draft"

--- src/App.jsx
+++ src/App.jsx
@@ -1,42 +1,34 @@
-import { useState, useEffect, useRef} from 'react';
-import AnimatedBackground from './components/AnimatedBackground';
+import { useState, useEffect } from 'react';
 import { getWeather } from './services/weatherService';
 import SearchBar from './components/SearchBar';
 import WeatherDisplay from './components/WeatherDisplay';
-
 
 const App = () => {
   const [weatherData, setWeatherData] = useState(null);
   const [city, setCity] = useState("New Jersey");
   const [searchCity, setSearchCity] = useState("");
-  const vantaRef = useRef(null)
   function handleSubmit(e) {
     e.preventDefault();
     setCity(searchCity);
   }
+
 
   // useEffect loads on initial render, and because city is in a dependency array the useEffect function will re run if the city value changes
   useEffect(() => {
     getWeather(city)
     .then((data) => setWeatherData(data))
     .catch((err) => console.log(err));
-  }, [city])
-
+  }, [city]);
   return (
-    <AnimatedBackground>
-  <div className="min-h-screen flex flex-col items-center px-4 py-8">
-  {/* // className="min-h-screen bg-linear-135 from-canva-lightBlue to-canva-darkBlue flex-col items-center px-4 py-8"> */}
-        <div className="w-full max-w-2x1 bg-white/20 backdrop-blur-md rounded-2xl p-6 shadow-xl">
-          <SearchBar
-            searchCity={searchCity}
-            setSearchCity={setSearchCity}
-            handleSubmit={handleSubmit}
-          />
-          <WeatherDisplay weatherData={weatherData} city={city}/>
-        </div>
-      </div>
-      </AnimatedBackground>
-  );
+    <div>
+      <SearchBar
+      searchCity={searchCity}
+      setSearchCity={setSearchCity}
+      handleSubmit={handleSubmit}
+      />
+     <WeatherDisplay weatherData={weatherData} city={city}/>
+    </div>
+  )
 }
 
 export default App
